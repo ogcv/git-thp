82d55181a1d6c791c0b10d35104bb16ce1eadf3d
Etienne <etienne.cor@gmail.com>
2019-10-01 12:53:13 +0200

    exo 1 a 5

--- exo_05.rb
+++ exo_05.rb
@@ -5,3 +5,18 @@ puts "En minutes ca fait : #{10*5*11*60}"
 puts "Et en seconde ?"
 
 puts 10 * 5 * 11 * 60 * 60 
+
+puts "Ect ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "Ca fait combien 3 + 3 ? #{3 + 2}"
+puts "Ca fait combien 5 - 7 ? #{5 - 7}"
+
+puts "OK, c'est faux alors !"
+
+puts "C'est drôle ca, faisons-en plus :"
+
+puts "Est ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est ce que 5 est supérieur ou égal à -2 ? #{5>= -2}"
+puts "Est ce que 5 est inférieur ou égal à -2 ? #{5<= -2}"
