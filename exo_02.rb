e5fb0c5e223c188f600d6ce07232e39fe56f49a0
Etienne <etienne.cor@gmail.com>
2019-10-01 11:49:42 +0200

    exo 2 a 5

---
+++ exo_02.rb
@@ -0,0 +1,2 @@
+puts "Hello world"
+puts "Et avec une vois sexy, ca donne : Bonjour, monde !"
---
+++ exo_05.rb
@@ -0,0 +1,7 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "travail : #{10*5*11}"
+puts "En minutes ca fait : #{10*5*11*60}"
+
+puts "Et en seconde ?"
+
+puts 10 * 5 * 11 * 60 * 60 
