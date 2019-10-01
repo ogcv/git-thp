5feba4739a75ceec29e84e573468cb519490cad8
Etienne <etienne.cor@gmail.com>
2019-10-01 14:55:58 +0200

    exo 12

---
+++ exo_11.rb
@@ -0,0 +1,5 @@
+puts "donne moi un chiffre"
+i = gets.chomp.to_i
+i.times do 
+	puts "salut ca farte"
+end
---
+++ exo_12.rb
@@ -0,0 +1,11 @@
+puts "donne moi un nombre"
+nb = gets.chomp.to_i
+total = 0 
+nb.times do 
+	total += 1
+	puts "total = " + total.to_s
+
+end
+
+	
+
