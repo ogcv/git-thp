50346c1e7fd15960b94c9582f315d80699490513
Etienne <etienne.cor@gmail.com>
2019-10-01 13:35:24 +0200

    exo 7 à 9

---
+++ exo_07c.rb
@@ -0,0 +1,3 @@
+user_name = gets.chomp
+puts user_name
+#puts fait sauter une ligne s'il y a plusieurs éléments, ce que print ne fait pas. 
---
+++ exo_09.rb
@@ -0,0 +1,6 @@
+puts "first name"
+first_name = gets.chomp
+puts "last-name"
+last_name = gets.chomp
+
+puts "Bonjour #{first_name} #{last_name}"
